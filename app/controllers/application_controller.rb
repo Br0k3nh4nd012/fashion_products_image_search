64c7a047c7d691a2125b6bd717071c43081e0d78
GK312 <gokulakrishnan312@gmail.com>
2023-07-08 18:10:25 +0530

    added few sample routes and form, to accept file image params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def home_page
+
+    end
+
+    def handle_form
+        puts params
+        flash[:error] = params.permit!.to_h
+        redirect_to root_path
+    end
 end
